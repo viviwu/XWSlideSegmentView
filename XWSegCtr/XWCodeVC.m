895fc323633f082b63c2ec3fd8b3209800856de2
viviwu <vivi705wu@sina.com>
2016-04-18 22:31:25 +0800

    addnewItemTitle With Code
    addnewItemTitle With Code

--- XWSegCtr/XWCodeVC.m
+++ XWSegCtr/XWCodeVC.m
@@ -25,18 +25,25 @@
     NSLog(@"%s---%ld", __func__, (long)index);
 }
 
+- (IBAction)addItem:(id)sender {
+    [_segCtr addnewItemTitle:@"NewItem"];
+}
+
 - (IBAction)initSegCtr:(id)sender {
+    if (nil==_segCtr) {
+        _segCtr=[[XWSegCtr alloc]initWithFrame:CGRectMake(0, 164.0, 320.0, 35.0) Titles:@[@"一", @"二", @"三", @"四",@"五"]];
+    }
     
-    _segCtr=[[XWSegCtr alloc]initWithFrame:CGRectMake(0, 164.0, 320.0, 35.0) Titles:@[@"一", @"二", @"三", @"四",@"五"]];
     _segCtr.backgroundColor=[UIColor yellowColor];
     _segCtr.titleColor=[UIColor redColor];
     _segCtr.titleSelectedColor=[UIColor purpleColor];
     _segCtr.touchDelegate=(id)self;
     [self.view addSubview:_segCtr];
-    //    _segCtr.titles= @[@"苹果",@"香蕉",@"橘子",@"葡萄",@"西瓜"];
+//        _segCtr.titles= @[@"苹果",@"香蕉",@"橘子",@"葡萄",@"西瓜"];
     _segCtr.selectedIndexHandle=^(NSInteger selectedIndex){
         NSLog(@"%s==%ld",__func__,  (unsigned long)selectedIndex);
     };
 }
+
 
 @end
